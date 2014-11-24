e46191749cf89795f1292225613a28c6d3171964
Egan Ford <datajerk@gmail.com>
2014-11-23 18:32:12 -0700

    added c2vip for Cosmac VIP

---
+++ c2vip.h
@@ -0,0 +1,13 @@
+
+const char *usagetext="\n\
+usage:  c2vip [-vh?]\n\
+        c2vip [-p]   inputfile ... [outputfile.mon]\n\
+        c2vip [-r #] inputfile ... [outputfile.[aif[f]|wav[e]]]\n\
+\n\
+        -h|? this help\n\
+        -p pipe to stdout\n\
+        -r #, where # overrides the sample rate (e.g. -r 48000)\n\
+        -v print version number and exit\n\
+\n\
+";
+
