a464aa014712e79885f8ba53bcb0045c48321d29
Egan Ford <datajerk@gmail.com>
2016-01-06 20:35:27 -0700

    merged windows cross build makefile with main makefile, updated test scripts and docs to support change

--- test.sh
+++ test.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+# Path to wine binary for testing Windows binary
+PATH=~/wine/bin:$PATH
+
+# list of commands to test, must be space delimited
+CMD=("./c2t-96h" "wine c2t-96h.exe")
+
+# list of images to test, must be space delimited.
+# Images cannot have spaces in names.
+IMAGES="zork.dsk dangerous_dave.po"
+
 dsk_test()
 {
+	IMAGE=$1
 	BASENAME=$(basename $1)
 	FILETYPE=$(echo $BASENAME | awk -F. '{print $NF}')
-	FILENAME=$(echo $BASENAME | sed "s/.$FILETYPE$//")
 
 	dd if=/dev/zero of=test.$FILETYPE bs=1k count=140 >/dev/null 2>&1
 
-	#./c2t-96h ${BASENAME} test.aif
-
 	osascript test.scrp test.${FILETYPE}
 
-	S1=$(md5sum ${BASENAME} | awk '{print $1}')
+	S1=$(md5sum ${IMAGE} | awk '{print $1}')
 	S2=$(md5sum test.$FILETYPE | awk '{print $1}')
 
-	echo "$BASENAME $S1 $S2"
+	echo "$IMAGE $S1 $S2"
 
 	if [ "$S1" = "$S2" ]
 	then
@@ -25,11 +33,7 @@ dsk_test()
 	return 1
 }
 
-PATH=~/wine/bin:$PATH
-
-CMD=("./c2t-96h" "wine c2t-96h.exe")
-
-for i in zork.dsk dangerous_dave.po
+for i in $IMAGES
 do
 	for j in $(seq 0 $(( ${#CMD[@]} - 1 )) )
 	do 
