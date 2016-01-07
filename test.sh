9d3f517423588bc5e03716cc885a7858f4c70986
Egan Ford <datajerk@gmail.com>
2016-01-07 10:10:09 -0700

    minor test clean up

--- test.sh
+++ test.sh
@@ -10,15 +10,27 @@ CMD=("./c2t-96h" "wine c2t-96h.exe")
 # Disks (and paths to disks) cannot have spaces in names.
 IMAGES="disks/zork.dsk disks/dangerous_dave.po"
 
-dsk_test()
+_test()
 {
 	IMAGE=$1
+	DONE=$2
 	BASENAME=$(basename $1)
 	FILETYPE=$(echo $BASENAME | awk -F. '{print $NF}')
 
 	dd if=/dev/zero of=test.$FILETYPE bs=1k count=140 >/dev/null 2>&1
 
-	osascript test.scrp test.${FILETYPE}
+	if ! OUTPUT=$(osascript test.scrp test.${FILETYPE} $DONE)
+	then
+		echo
+		return 1
+	fi
+
+	if echo $OUTPUT | grep ERROR >/dev/null 2>&1
+	then
+		echo $OUTPUT
+		echo
+		return 1
+	fi
 
 	S1=$(md5sum ${IMAGE} | awk '{print $1}')
 	S2=$(md5sum test.$FILETYPE | awk '{print $1}')
@@ -39,7 +51,7 @@ do
 	do 
 		echo ${CMD[$j]} $i test.aif
 		eval ${CMD[$j]} $i test.aif
-		if dsk_test $i
+		if _test $i dskappleiie.tiff
 		then
 			echo "$i passed"
 			echo
